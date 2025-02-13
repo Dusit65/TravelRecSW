e00779d56c1b6d8d6bd2da6a1c19a9397999ac6d
Dusit Pakdeekunakorn <S6552410011@sau.ac.th>
2025-02-13 12:24:14 +0700

    V1

--- TravelRecSW/FrmLogin.cs
+++ TravelRecSW/FrmLogin.cs
@@ -13,9 +13,89 @@ namespace TravelRecSW
 {
     public partial class FrmLogin : Form
     {
+        
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void lbToFrmRegister_Click(object sender, EventArgs e)
+        {
+            FrmRegister frmRegister = new FrmRegister();
+            frmRegister.Show();
+            this.Hide();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            //validate UI
+            if(tbTravellerEmail.Text.Trim().Length == 0)
+            {
+                ShareInfo.showWarningMSG("กรุณากรอกอีเมล");
+            }
+            else if (tbTravellerPassword.Text.Trim().Length == 0)
+            {
+                ShareInfo.showWarningMSG("กรุณากรอกรหัสผ่าน");
+            }
+            else
+            {
+                //Save to DB
+                //connect to DB
+                SqlConnection conn = new SqlConnection(ShareInfo.conStr);
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn.Open();
+                //sql command
+                string strSql = "SELECT * FROM traveller_tb WHERE " +
+                                "travellerEmail = @travellerEmail and " + 
+                                "travellerPassword = @travellerPassword";
+
+                //create sql transaction and sql command for working with SQL
+                SqlTransaction sqlTransaction = conn.BeginTransaction();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = conn;
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.CommandText = strSql;
+                sqlCommand.Transaction = sqlTransaction;
+
+                //bindParam
+                sqlCommand.Parameters.AddWithValue("@travellerEmail", tbTravellerEmail.Text.Trim());
+                sqlCommand.Parameters.AddWithValue("@travellerPassword", tbTravellerPassword.Text.Trim());
+
+                //run SQL
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                conn.Close();
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0) 
+                {
+                    //packing data to shareInfo
+                    ShareInfo.travellerId = (int)dt.Rows[0]["travellerId"];
+                    ShareInfo.travellerFullname = dt.Rows[0]["travellerFullname"].ToString();
+                    ShareInfo.travellerEmail = dt.Rows[0]["travellerEmail"].ToString();
+                    ShareInfo.travellerPassword = dt.Rows[0]["travellerPassword"].ToString();
+                    ShareInfo.travellerImage = (byte)dt.Rows[0]["travellerImage"];
+
+
+                    //incase login success
+                    FrmTravelOpt frmMain = new FrmTravelOpt();
+                    frmMain.Show();
+                    Hide();
+                }
+                else
+                {
+                    //incase login fail
+                    sqlTransaction.Rollback();
+                    
+                    ShareInfo.showWarningMSG("อีเมลหรือรหัสผ่านไม่ถูกต้อง");
+                }
+
+
+            }
         }
     }
 }
--- TravelRecSW/FrmRegister.Designer.cs
+++ TravelRecSW/FrmRegister.Designer.cs
@@ -58,9 +58,10 @@
             this.label1.BackColor = System.Drawing.Color.Yellow;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(32, 28);
+            this.label1.Location = new System.Drawing.Point(43, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(509, 48);
+            this.label1.Size = new System.Drawing.Size(679, 59);
             this.label1.TabIndex = 2;
             this.label1.Text = "ลงทะเบียน";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -68,94 +69,108 @@
             // btSelectTravellerImage
             // 
             this.btSelectTravellerImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btSelectTravellerImage.Location = new System.Drawing.Point(87, 313);
+            this.btSelectTravellerImage.Location = new System.Drawing.Point(116, 385);
+            this.btSelectTravellerImage.Margin = new System.Windows.Forms.Padding(4);
             this.btSelectTravellerImage.Name = "btSelectTravellerImage";
-            this.btSelectTravellerImage.Size = new System.Drawing.Size(136, 34);
+            this.btSelectTravellerImage.Size = new System.Drawing.Size(181, 42);
             this.btSelectTravellerImage.TabIndex = 3;
             this.btSelectTravellerImage.Text = "Select your image";
             this.btSelectTravellerImage.UseVisualStyleBackColor = true;
+            this.btSelectTravellerImage.Click += new System.EventHandler(this.btSelectTravellerImage_Click);
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.pbTravellerImage);
-            this.panel1.Location = new System.Drawing.Point(83, 110);
+            this.panel1.Location = new System.Drawing.Point(111, 135);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(145, 187);
+            this.panel1.Size = new System.Drawing.Size(193, 230);
             this.panel1.TabIndex = 4;
             // 
             // pbTravellerImage
             // 
             this.pbTravellerImage.Image = global::TravelRecSW.Properties.Resources.profile;
-            this.pbTravellerImage.Location = new System.Drawing.Point(3, 3);
+            this.pbTravellerImage.Location = new System.Drawing.Point(4, 4);
+            this.pbTravellerImage.Margin = new System.Windows.Forms.Padding(4);
             this.pbTravellerImage.Name = "pbTravellerImage";
-            this.pbTravellerImage.Size = new System.Drawing.Size(136, 179);
+            this.pbTravellerImage.Size = new System.Drawing.Size(181, 220);
             this.pbTravellerImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbTravellerImage.TabIndex = 1;
             this.pbTravellerImage.TabStop = false;
             // 
             // tbTravellerEmail
             // 
-            this.tbTravellerEmail.Location = new System.Drawing.Point(299, 185);
+            this.tbTravellerEmail.Location = new System.Drawing.Point(399, 228);
+            this.tbTravellerEmail.Margin = new System.Windows.Forms.Padding(4);
             this.tbTravellerEmail.Name = "tbTravellerEmail";
-            this.tbTravellerEmail.Size = new System.Drawing.Size(188, 20);
+            this.tbTravellerEmail.Size = new System.Drawing.Size(249, 22);
             this.tbTravellerEmail.TabIndex = 9;
             // 
             // tbTravellerFullname
             // 
-            this.tbTravellerFullname.Location = new System.Drawing.Point(299, 137);
+            this.tbTravellerFullname.Location = new System.Drawing.Point(399, 169);
+            this.tbTravellerFullname.Margin = new System.Windows.Forms.Padding(4);
             this.tbTravellerFullname.Name = "tbTravellerFullname";
-            this.tbTravellerFullname.Size = new System.Drawing.Size(188, 20);
+            this.tbTravellerFullname.Size = new System.Drawing.Size(249, 22);
             this.tbTravellerFullname.TabIndex = 8;
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(299, 161);
+            this.label3.Location = new System.Drawing.Point(399, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 20);
+            this.label3.Size = new System.Drawing.Size(212, 25);
             this.label3.TabIndex = 7;
             this.label3.Text = "อีเมล์";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(299, 113);
+            this.label2.Location = new System.Drawing.Point(399, 139);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(159, 20);
+            this.label2.Size = new System.Drawing.Size(212, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "ชื่อ - สกุล";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // tbTravellerPasswordConfirm
             // 
-            this.tbTravellerPasswordConfirm.Location = new System.Drawing.Point(299, 281);
+            this.tbTravellerPasswordConfirm.Location = new System.Drawing.Point(399, 346);
+            this.tbTravellerPasswordConfirm.Margin = new System.Windows.Forms.Padding(4);
             this.tbTravellerPasswordConfirm.Name = "tbTravellerPasswordConfirm";
             this.tbTravellerPasswordConfirm.PasswordChar = '*';
-            this.tbTravellerPasswordConfirm.Size = new System.Drawing.Size(188, 20);
+            this.tbTravellerPasswordConfirm.Size = new System.Drawing.Size(249, 22);
             this.tbTravellerPasswordConfirm.TabIndex = 13;
+            this.tbTravellerPasswordConfirm.Enter += new System.EventHandler(this.tbTravellerPasswordConfirm_Enter);
             // 
             // tbTravellerPassword
             // 
-            this.tbTravellerPassword.Location = new System.Drawing.Point(299, 233);
+            this.tbTravellerPassword.Location = new System.Drawing.Point(399, 287);
+            this.tbTravellerPassword.Margin = new System.Windows.Forms.Padding(4);
             this.tbTravellerPassword.Name = "tbTravellerPassword";
             this.tbTravellerPassword.PasswordChar = '*';
-            this.tbTravellerPassword.Size = new System.Drawing.Size(188, 20);
+            this.tbTravellerPassword.Size = new System.Drawing.Size(249, 22);
             this.tbTravellerPassword.TabIndex = 12;
+            this.tbTravellerPassword.Enter += new System.EventHandler(this.tbTravellerPassword_Enter);
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(299, 257);
+            this.label4.Location = new System.Drawing.Point(399, 316);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 20);
+            this.label4.Size = new System.Drawing.Size(212, 25);
             this.label4.TabIndex = 11;
             this.label4.Text = "ยืนยันรหัสผ่าน";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(299, 209);
+            this.label5.Location = new System.Drawing.Point(399, 257);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(159, 20);
+            this.label5.Size = new System.Drawing.Size(212, 25);
             this.label5.TabIndex = 10;
             this.label5.Text = "รหัสผ่าน";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -164,15 +179,16 @@
             // 
             this.toolStrip1.AutoSize = false;
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsbtSave,
             this.tsbtCancel,
             this.toolStripLabel1,
             this.toolStripSeparator1,
             this.tsbtToFrmLogin});
-            this.toolStrip1.Location = new System.Drawing.Point(567, 0);
+            this.toolStrip1.Location = new System.Drawing.Point(756, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(128, 376);
+            this.toolStrip1.Size = new System.Drawing.Size(171, 463);
             this.toolStrip1.TabIndex = 14;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -181,55 +197,59 @@
             this.tsbtSave.Image = global::TravelRecSW.Properties.Resources.save;
             this.tsbtSave.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbtSave.Name = "tsbtSave";
-            this.tsbtSave.Size = new System.Drawing.Size(126, 20);
+            this.tsbtSave.Size = new System.Drawing.Size(169, 24);
             this.tsbtSave.Text = "บันทึก";
             this.tsbtSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.tsbtSave.Click += new System.EventHandler(this.tsbtSave_Click);
             // 
             // tsbtCancel
             // 
             this.tsbtCancel.Image = global::TravelRecSW.Properties.Resources.cancel;
             this.tsbtCancel.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbtCancel.Name = "tsbtCancel";
-            this.tsbtCancel.Size = new System.Drawing.Size(126, 20);
+            this.tsbtCancel.Size = new System.Drawing.Size(169, 24);
             this.tsbtCancel.Text = "ยกเลิก";
             this.tsbtCancel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.tsbtCancel.Click += new System.EventHandler(this.tsbtCancel_Click);
             // 
             // toolStripLabel1
             // 
             this.toolStripLabel1.Name = "toolStripLabel1";
             this.toolStripLabel1.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never;
-            this.toolStripLabel1.Size = new System.Drawing.Size(126, 0);
+            this.toolStripLabel1.Size = new System.Drawing.Size(169, 0);
             this.toolStripLabel1.TextDirection = System.Windows.Forms.ToolStripTextDirection.Vertical90;
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(126, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(169, 6);
             // 
             // tsbtToFrmLogin
             // 
             this.tsbtToFrmLogin.Image = global::TravelRecSW.Properties.Resources.logo;
             this.tsbtToFrmLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbtToFrmLogin.Name = "tsbtToFrmLogin";
-            this.tsbtToFrmLogin.Size = new System.Drawing.Size(126, 20);
+            this.tsbtToFrmLogin.Size = new System.Drawing.Size(169, 24);
             this.tsbtToFrmLogin.Text = "เข้าใช้งานระบบ";
             this.tsbtToFrmLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.tsbtToFrmLogin.Click += new System.EventHandler(this.tsbtToFrmLogin_Click);
             // 
             // cbConfirm
             // 
             this.cbConfirm.AutoSize = true;
-            this.cbConfirm.Location = new System.Drawing.Point(334, 323);
+            this.cbConfirm.Location = new System.Drawing.Point(445, 398);
+            this.cbConfirm.Margin = new System.Windows.Forms.Padding(4);
             this.cbConfirm.Name = "cbConfirm";
-            this.cbConfirm.Size = new System.Drawing.Size(124, 17);
+            this.cbConfirm.Size = new System.Drawing.Size(121, 20);
             this.cbConfirm.TabIndex = 15;
             this.cbConfirm.Text = "ยืนยันการลงทะเบียน";
             this.cbConfirm.UseVisualStyleBackColor = true;
             // 
             // FrmRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(695, 376);
+            this.ClientSize = new System.Drawing.Size(927, 463);
             this.Controls.Add(this.cbConfirm);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.tbTravellerPasswordConfirm);
@@ -245,6 +265,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "FrmRegister";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
